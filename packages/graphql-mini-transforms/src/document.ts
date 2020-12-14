f6d9023055fdce490ed71c0e96448368ebebbde8
David Sanders <dsanders11@ucsbalum.com>
2020-12-14 15:34:23 -0800

    Apply code review change

--- packages/graphql-mini-transforms/src/document.ts
+++ packages/graphql-mini-transforms/src/document.ts
@@ -16,6 +16,13 @@ import {DocumentNode, SimpleDocument} from 'graphql-typed';
 const IMPORT_REGEX = /^#import\s+['"]([^'"]*)['"];?[\s\n]*/gm;
 const DEFAULT_NAME = 'Operation';
 
+function defaultGenerateId(normalizedSource: string) {
+  // This ID is a hash of the full file contents that are part of the document,
+  // including other documents that are injected in, but excluding any unused
+  // fragments. This is useful for things like persisted queries.
+  return createHash('sha256').update(normalizedSource).digest('hex');
+}
+
 export interface CleanDocumentOptions {
   removeUnused?: boolean;
   generateId?: (normalizedSource: string) => string;
@@ -41,18 +48,10 @@ export function cleanDocument(
     stripLoc(definition);
   }
 
-  let id: string;
-
-  if (generateId === undefined) {
-    // This ID is a hash of the full file contents that are part of the document,
-    // including other documents that are injected in, but excluding any unused
-    // fragments. This is useful for things like persisted queries.
-    id = createHash('sha256')
-      .update(minifySource(normalizedSource))
-      .digest('hex');
-  } else {
-    id = generateId(documentSource);
-  }
+  const id =
+    generateId === undefined
+      ? defaultGenerateId(normalizedSource)
+      : generateId(documentSource);
 
   Reflect.defineProperty(normalizedDocument, 'id', {
     value: id,
